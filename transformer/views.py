dc4c70408006f72e64538286ee2a12ca504da99e
Fcisraelalves <alves.israel@aluno.ifce.edu.br>
2026-04-04 01:14:37 -0300

    refactor: criar uma função genérica para realizar as divisões sucessivas
    Criei uma função genérica que recebe como parâmetro uma determinada base e um determinado valor e realiza as divisões sucessivas, retornando os restos para que a conversão seja possível.

--- transformer/views.py
+++ transformer/views.py
@@ -4,6 +4,25 @@ from rest_framework.response import Response
 from rest_framework import status
 # Create your views here.
 
+def _sucessive_divisions(base : int, value : int):
+    rests = []
+
+    while True:
+        
+        quotient = value // base
+        rest = value % base
+
+        rests.append(rest)
+        value = quotient
+
+        if quotient < base:
+            break
+    
+    rests.append(quotient)
+
+    return rests
+
+
 @api_view(['GET'])
 def decimal_to_binary(request):
     
@@ -20,18 +39,7 @@ def decimal_to_binary(request):
         return Response(data={'error' : 'The value must be a number'},
                         status=status.HTTP_400_BAD_REQUEST)
     
-    rests = []
-    while True:
-        
-        quotient = value // 2
-        rest = value % 2
-
-        rests.append(rest)
-        value = quotient
-
-        if quotient == 1:
-            break
-    rests.append(1)
+    rests = _sucessive_divisions(2, value)
 
     binary = ''.join([str(digit) for digit in rests[::-1]])
 
